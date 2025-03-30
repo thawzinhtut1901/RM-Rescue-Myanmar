5a704d3de4330e8911233952e19d1a3979cb326c
zink9169 <117247853+zink9169@users.noreply.github.com>
2025-03-30 23:55:17 +0630

    Home Page UI

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import homeImage from "../assets/images/home.png";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="flex flex-col md:flex-row items-center justify-center p-4 md:p-8">
+      <div className="flex flex-col md:flex-row items-center bg-white p-6 md:p-10 rounded-lg max-w-5xl w-full">
+        <img
+          src={homeImage}
+          alt="Emergency Kit"
+          className="w-full md:w-1/2 h-auto object-contain mb-4 md:mb-0 md:mr-6"
+        />
+        <div>
+          <h2 className="text-red-600 font-bold text-xl md:text-2xl">
+            Emergency Earthquake Response – Connect, Respond, Save Lives
+          </h2>
+          <p className="text-gray-700 mt-2 md:mt-4 text-base md:text-lg">
+            Instantly connect with volunteer rescue teams and emergency services
+            during earthquakes.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import { useNavigate } from 'react-router'
+import React from "react";
+import { useNavigate } from "react-router";
+import logo from "../assets/images/logo.png";
 
 const Navigation = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
-    <div className='flex justify-between items-center bg-red-600 w-full h-[60px] text-white'>
-        <h1>RM - Rescue Myanmar</h1>
+    <div className="flex justify-around items-center w-full h-[60px] text-white">
+      <div className="flex items-center">
+        <img src={logo} alt="logo" className="w-10 h-10 rounded-full ml-6" />
+      </div>
 
-        <div className='flex gap-x-10 mx-4 cursor-pointer'>
-            <h1 onClick={() => navigate("/home")}>Home</h1>
-            <h1 onClick={() => navigate("/emergency-contacts")}>Emergency Contacts</h1>
-            <h1 onClick={() => navigate("/free-restaurant")}>Free Restaurant</h1>
-        </div>
+      <div className="flex gap-x-10 mx-4 cursor-pointer border border-red-500 rounded-full px-4 py-2 text-black">
+        <h1 onClick={() => navigate("/home")}>Home</h1>
+        <h1 onClick={() => navigate("/emergency-contacts")}>
+          Emergency Contacts
+        </h1>
+        <h1 onClick={() => navigate("/free-restaurant")}>Free Restaurant</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
