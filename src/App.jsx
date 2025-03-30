86a39d215056f13d30e5fa3818ee5c89aed95ef4
thawzinhtut <thawzinhtut9421@gmail.com>
2025-03-29 22:46:21 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react'
+import emergency_contacts from "./data/contact.json"
+
+const App = () => {
+  console.log(emergency_contacts)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
