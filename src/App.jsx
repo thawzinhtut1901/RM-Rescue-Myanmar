775d36c82dcf10ff5913a6db9252d531da420af8
zink9169 <117247853+zink9169@users.noreply.github.com>
2025-03-30 22:28:08 +0630

    Saved local changes before pulling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react'
-import emergency_contacts from "./data/contact.json"
+import React, { useEffect, useState } from "react";
+import emergency_contacts from "./data/contact.json";
+import ContactList from "./pages/personList";
+import EmergencyContactList from "./pages/personList";
 
 const App = () => {
-  console.log(emergency_contacts)
+  console.log(emergency_contacts);
   return (
     <div>
-      
+      <EmergencyContactList />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
