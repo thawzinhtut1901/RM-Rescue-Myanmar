eda81de9e155aef6750898fba676ef47bc90fe6c
thawzinhtut <thawzinhtut9421@gmail.com>
2025-03-31 00:26:34 -0700

    Home Page Fix

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -3,20 +3,20 @@ import homeImage from "../assets/images/home.png";
 
 const Home = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center p-4 md:p-8">
-      <div className="flex flex-col md:flex-row items-center bg-white p-6 md:p-10 rounded-lg max-w-5xl w-full">
+    <div className="flex md:flex-row flex-col justify-center items-center p-4 md:p-8">
+      <div className="flex md:flex-row flex-col items-center bg-white p-6 md:p-10 rounded-lg w-full max-w-5xl">
         <img
           src={homeImage}
           alt="Emergency Kit"
-          className="w-full md:w-1/2 h-auto object-contain mb-4 md:mb-0 md:mr-6"
+          className="md:mr-6 mb-4 md:mb-0 w-full md:w-1/2 h-auto object-contain"
         />
         <div>
-          <h2 className="text-red-600 font-bold text-xl md:text-2xl">
-            Emergency Earthquake Response – Connect, Respond, Save Lives
+          <h2 className="font-bold text-red-600 text-xl md:text-2xl">
+            Emergency Natural Disasters Response – Connect, Respond, Save Lives
           </h2>
-          <p className="text-gray-700 mt-2 md:mt-4 text-base md:text-lg">
+          <p className="mt-2 md:mt-4 text-gray-700 text-base md:text-lg">
             Instantly connect with volunteer rescue teams and emergency services
-            during earthquakes.
+            during natural disasters.
           </p>
         </div>
       </div>
